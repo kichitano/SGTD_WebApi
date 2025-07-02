582f639567e66ee3f77f157aaf789cb376506891
kichitano <kixi.13.7@gmail.com>
2025-07-01 21:01:07 -0500

    .

--- Models/DocumentaryProcess/DocumentaryProcessRequestParams.cs
+++ Models/DocumentaryProcess/DocumentaryProcessRequestParams.cs
@@ -11,13 +11,10 @@ public class DocumentaryProcessRequestParams
     
     public Guid? UserGuid { get; set; }
 
-    [Required]
     public int DocumentaryProcessId { get; set; }
 
-    [Required]
     public string Name { get; set; }
 
-    [StringLength(500)]
     public string Description { get; set; }
 
     public bool Status { get; set; } = true;
